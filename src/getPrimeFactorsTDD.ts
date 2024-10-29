b83fefdbe18b18449c97a055368bef03c39693f6
Ting Li <TLi26@slb.com>
2024-10-29 16:05:55 +0800

    feat: implement input = 3

--- src/getPrimeFactorsTDD.ts
+++ src/getPrimeFactorsTDD.ts
@@ -3,5 +3,8 @@ export function getPrimeFactorsTDD(num: number): number[] {
   if (num % 2 == 0) {
     primeFactors.push(2);
   }
+  if (num % 3 == 0) {
+    primeFactors.push(3);
+  }
   return primeFactors;
 }
--- test/getPrimeFactorTDD.test.ts
+++ test/getPrimeFactorTDD.test.ts
@@ -3,7 +3,8 @@ import { getPrimeFactorsTDD } from "../src/getPrimeFactorsTDD";
 describe("add", () => {
   const testCases = [
     { num: 1, expected: [] },
-    { num: 2, expected: [2] }
+    { num: 2, expected: [2] },
+    { num: 3, expected: [3] }
   ];
   testCases.forEach(({ num, expected }) => {
     it(`getPrimeFactors(${num}) should return ${expected}`, () => {
